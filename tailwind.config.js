c07c5f47ebe325cc53c954912b6c8af44e8cdb40
worod9934 <159957430+worod9934@users.noreply.github.com>
2024-11-02 02:05:24 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/Components/Footer/Footer.jsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#f7ba34",
+        secondary: "#69a79c",
+        light: "#f7f7f7",
+        dark: "#333333",
+        dark2: "#999999",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
